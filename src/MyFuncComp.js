acbc1fecd3f6d76f03641ea506dae5eee4de35d1
Sheldon Bai <sheldon.bai1989@gmail.com>
2026-02-21 11:53:16 -0600

    The init state

--- src/MyFuncComp.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function MyFuncComp(props) {
-    return <h1>abcde, my number: {props.number}</h1>
-}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Student from './components/Student'
-import Students from './components/Students'
-
-const student1 = {
-  id: 1,
-  name: 'Xudong',
-  email: 'sheldon.bai1989@gmail.com',
-  gender: 'Male',
-}
-
-const student2 = {
-  id: 2,
-  name: 'Bella',
-  email: 'Bellayang3600@gmail.com',
-  gender: 'Female',
-}
-const stus = [
-  student1,
-  student2
-]
-
-console.log(stus)
 
 function App() {
   return (
     <>
-      <Students stus={stus}></Students>
+      <div>A react app</div>
     </>
   )
 }
