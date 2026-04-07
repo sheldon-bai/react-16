2dadb14632c7da8307546242301973723763ed40
Sheldon Bai <sheldon.bai1989@gmail.com>
2026-04-07 11:24:54 -0500

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Modal from './components/common/Modal'
 
-function App() {
-  return (
-    <>
-      <div>A react app</div>
-    </>
-  )
+class App extends Component {
+  state = {
+    showModal: false,
+  }
+
+  showModal = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  hideModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <div style={{
+          width: '100px',
+          height: '100px',
+          background: 'red',
+        }}>Content</div>
+        {
+          this.state.showModal ? (
+            <Modal onClose={this.hideModal}>
+              <div>Children abc</div>
+              <button onClick={this.hideModal}>Close the Modal</button>
+            </Modal>
+          ) : null
+        }
+        <button onClick={this.showModal}>Display the Modal</button>
+      </>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
