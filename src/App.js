65ca5a5cf33e26ddce9759ee3053d933838983fe
Sheldon Bai <sheldon.bai1989@gmail.com>
2026-04-20 22:34:28 -0500

    Add App change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import TaskContainer from './components/TaskContainer'
 
 export default function App() {
     return (
-        <div>App</div>
+        <TaskContainer />
     )
 }
