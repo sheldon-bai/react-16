905a64befd2b450f7aece3fe0130125ccb0b70fa
Sheldon Bai <sheldon.bai1989@gmail.com>
2026-04-23 09:32:59 -0500

    Add render props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react'
+// import MovablePanel from './component/MovablePanel'
+// import ShowMousePoint from './component/ShowMousePoint'
+import MouseListener from './component/MouseListener'
+
+const renderPoint = mouse => (
+                    <h2>
+                        鼠标x: {parseInt(mouse.x)}
+                        鼠标y: {parseInt(mouse.y)}
+                    </h2>
+                )
+const renderDiv = mouse => (
+                    <div style={{
+                        width: 100,
+                        height: 100,
+                        background: '#008c8c',
+                        position: 'absolute',
+                        left: mouse.x - 50,
+                        top: mouse.y - 50,
+                    }}></div>
+                ) 
 
 export default function App() {
     return (
-        <div>App</div>
+        <>
+            <MouseListener>
+                {renderPoint}
+            </MouseListener>
+            <MouseListener>
+                {renderDiv}
+            </MouseListener>
+        </>
     )
 }
